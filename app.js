8d655cb46df66cb7fb1ee44355c4777c07fbb07f
Igor Oliveira <57597874+olivigora@users.noreply.github.com>
2021-05-02 07:37:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.computer-grid')
+    const displayGrid = document.querySelector('.grid-display')
+    const ships = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
+    const startButton = document.querySelector(#start)
+    const rotateButton = document.querySelector(#rotate)
+    const turnDisplay = document.querySelector(#whose-go)
+    const infoDisplay = document.querySelector(#info)
+
+    const width = 10 
+
+    //Create board
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            document.
+        }
+
+    }
+
+
+})
